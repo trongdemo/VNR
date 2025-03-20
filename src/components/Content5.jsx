f6311333e74cae8b4163fe81d7c03c9bfa1b3131
tronglhse161154 <tronglhse161154@fpt.edu.vn>
2025-03-20 22:22:40 +0700

    fix

--- src/components/Content5.jsx
+++ src/components/Content5.jsx
@@ -4,8 +4,7 @@ const Content5 = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20 p-10 bg-[#FEEED2] rounded-lg">
       <h2
-        className="text-4xl font-bold text-black text-center mb-6"
-        style={{ fontFamily: "iCielBC Lodestone, sans-serif" }}
+        className="text-4xl font-bold text-black text-center mb-6 lodestone"
       >
         Sách online từ bao cấp tới đổi mới đất nước
       </h2>
